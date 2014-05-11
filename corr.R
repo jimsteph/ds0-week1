5309f2b191733f044861ff413f3694cb1e32fce1
Jim Stephenson <azmactech@gmail.com>
2014-05-10 23:34:58 -0700

    Completed corr(): passed all unit tests

--- corr.R
+++ corr.R
@@ -8,7 +8,7 @@
 #			path to the directory and 'nnn' is 'id' padded with leading zeros.
 #
 getfilename <- function(directory, i) {
-  fname <- as(i, "character")
+fname <- as(i, "character")
   flength <- nchar(fname)
   if(flength == 1) fname <- paste(directory, "/", "00", fname, ".csv", sep="")
   else
@@ -28,5 +28,30 @@ getfilename <- function(directory, i) {
 # Output: 	A numeric vector of correlations
 #
 corr <- function(directory, threshold = 0) {
+  # Initialize variables
+  cors <- vector(mode="numeric", length=0)
+  loopcount <- 0
   
+  # Loop through all files
+  id=1:332
+  for(i in id) {
+    loopcount <- loopcount + 1
+    comp <- read.csv(getfilename(directory, id[loopcount]))
+    
+    # See if current file has at least 'threshold' complete frames
+    # First, get the set of complete cases in the file
+    ccases <- 0
+    for(i in 1:nrow(comp)) {
+      if(!is.na(comp[i,2]) & !is.na(comp[i,3])) {
+        ccases<-ccases + 1
+      }
+    }
+    # if it passes the threshold, process it
+    if(ccases>threshold) {
+      ccc<-cor(comp[,2], comp[,3], use="na.or.complete")
+      cors <- append(cors, ccc)
+    }
+  }
+  # Return value
+  cors
 }
